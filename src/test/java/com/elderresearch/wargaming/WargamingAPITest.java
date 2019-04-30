c2e2ff9e247c58b62909f10a5b0a6530d2329eca
John Dimeo <dimeo@elderresearch.com>
2019-04-30 14:41:44 -0400

    Set key so unit test can run

--- src/test/java/com/elderresearch/wargaming/WargamingAPITest.java
+++ src/test/java/com/elderresearch/wargaming/WargamingAPITest.java
@@ -4,11 +4,16 @@ import com.elderresearch.wargaming.WargamingAPI.ClanAPI;
 import com.elderresearch.wargaming.WargamingAPI.ClansAPI;
 import com.elderresearch.wargaming.WargamingAPI.PageParams;
 import com.elderresearch.wargaming.WargamingAPI.VehiclesAPI;
+import com.elderresearch.wargaming.WargamingConfig.WargamingOption;
 
 import junit.framework.TestCase;
 
 public class WargamingAPITest extends TestCase {
-
+	@Override
+	protected void setUp() throws Exception {
+		WargamingOption.WARGAMING_API_KEY.set("ef14d5a269a9c30f5803bbc03cc2d235");
+	}
+	
 	public void testClans() {
 		System.out.println(ClansAPI.get(PageParams.of(1, 5)).getData().get(0));
 	}
